a2054c660b86156c2fb83479c6389c502655ed76
Feliz Salting <fpsalting@student.apc.edu.ph>
2025-02-26 15:15:05 +0000

    re-nap

--- supabaseclient.js
+++ supabaseclient.js
@@ -1,5 +1,8 @@
-// Ensure Supabase is loaded from the CDN
-const supabase = window.supabase.createClient(
-    'https://kxoexwdhzcxmxzygwatq.supabase.co', // Replace with your Supabase URL
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4b2V4d2RoemN4bXh6eWd3YXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MDk4MjAsImV4cCI6MjA1NTI4NTgyMH0.wXpVEFOOHHReYwJj6cDltmjy3Ff5MXG75lFhA_8xS78' // Replace with your API Key
-);
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://kxoexwdhzcxmxzygwatq.supabase.co'; // Replace with your Supabase Project URL
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4b2V4d2RoemN4bXh6eWd3YXRxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzk3MDk4MjAsImV4cCI6MjA1NTI4NTgyMH0.wXpVEFOOHHReYwJj6cDltmjy3Ff5MXG75lFhA_8xS78'; // Replace with your Supabase API Key
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
